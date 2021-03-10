f73acd39eca3d1fcab4df64b3abe3db2ea3be2d6
Tom Storms <tom@islandmeetscity.com>
2021-03-10 16:17:32 +1000

    Code cleanup

--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -1,8 +1,4 @@
-import React, { useContext } from 'react'
-import { myContext } from '../Context/Context'
-
 export default function HomePage() {
-    const ctx = useContext(myContext);
     return (
         <div>
             <h1>HomePage</h1>
